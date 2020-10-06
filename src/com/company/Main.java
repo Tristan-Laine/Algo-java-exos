9743eeae78759821222836e362f9fac0f00cc429
Tristan Laine <tristan.laine@edu.itescia.fr>
2020-10-06 13:32:15 +0200

    TP3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,27 @@
 package com.company;
 
 public class Main {
+    /**
+     * Fonction qui renvoie le score du tableau
+     * @param Score
+     */
 
     //Fonction qui renvoie le score
-    static void FonctionScore (int Tableau []) {            //Création fonctionScore
-        for (int i = 0; i < Tableau.length; i++) {          //Création boucle for
-            int score1 = Tableau[i];                        //Score1 prend la valeur du tableau en i
+    public static void FonctionScore (int [] Score ) {            //Création fonctionScore
+        for (int i = 0; i < Score.length; i++) {          //Création boucle for
+            int score1 = Score[i];                        //Score1 prend la valeur du tableau en i
 
             System.out.println("Fonction score " + (i + 1) + " = " + score1); //on dis le score
         }
     }
 
-        //Fonction qui renvoie le score moyen
-        static void FonctionMoyenne (int Moy []){           //Création fonctionMoyenne
+    /**
+     * Fonction qui renvoie le score moyen du tableau
+     * @param Moy
+     */
+
+    //Fonction qui renvoie le score moyen
+    public static void FonctionMoyenne (int [] Moy ){           //Création fonctionMoyenne
             int moyenne = 0;                                //on crée la variable moyenne
             for (int i = 0; i < Moy.length; i++) {          //Création boucle for
                 moyenne = moyenne + Moy[i];                 //la varible moyenne prend sa valeur + la valeur Moy en i
@@ -21,8 +30,14 @@ public class Main {
             System.out.println("Fonction Moyenne = " +moyenne); //on dis la moyenne
         }
 
-        //Fonction qui renvoie le plus grand nombre
-        static int FonctionPlusGrandNB (int Nombre []){     //Création fonctionPlusGrandNB
+    /**
+     * Fonction qui renvoie le plus grand nombre du tableau
+     * @param Nombre
+     * @return
+     */
+
+    //Fonction qui renvoie le plus grand nombre
+    public static int FonctionPlusGrandNB (int [] Nombre ){     //Création fonctionPlusGrandNB
         int PlusGrandNB = 0;                                //on crée la variable PlusGrandNB
             for (int i = 0; i < Nombre.length; i++) {       //Création boucle for
                 if (PlusGrandNB < Nombre[i]){               //si PlusGrandNB plus petit que Nombre[i] (valeur du tableau en i)
@@ -32,8 +47,14 @@ public class Main {
         return PlusGrandNB;                                 //on return PlusGrandNB
         }
 
-        //Fonction qui renvoie true si une valeur est plus petite que 10
-        static boolean FonctionValeur10 (int Valeur []){    //Création FonctionValeur10
+    /**
+     * Fonction qui renvoie true si une valeur est plus petite que 10
+     * @param Valeur
+     * @return
+     */
+
+    //Fonction qui renvoie true si une valeur est plus petite que 10
+    public static boolean FonctionValeur10 (int [] Valeur ){    //Création FonctionValeur10
             boolean BoolValeur = false ;                    //Création du boolean BoolValeur
             for (int i = 0; i < Valeur.length; i++){        //Création d'une boucle for
                 if (Valeur[i] < 10){                        //si la valeur en i du tableau est plus petit que 10
---
+++ src/com/company/TP4.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class TP4 {
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+
+        //Création d'un Tableau scores
+        int[] scores = new int[7];
+        scores[0] = 10;
+        scores[1] = 20;
+        scores[2] = 30;
+        scores[3] = 40;
+        scores[4] = 50;
+        scores[5] = 60;
+        scores[6] = 70;
+
+
+
+
+
+
+    }
+}
