3eb0501da54d4ebc8d0e0022f9e78f58f3c7d1a6
Tristan Laine <tristan.laine@edu.itescia.fr>
2020-10-07 09:53:30 +0200

    fin tp5

--- src/com/company/TP5.java
+++ src/com/company/TP5.java
@@ -2,7 +2,32 @@ package com.company;
 
 public class TP5 {
 
-    
+    /**
+     * Fonction qui renvoie l'index de la case contenant
+     * le nombre demandé (en recherche binaire) dans un tableau
+     *
+     * @param Tableau
+     * @param A
+     * @return
+     */
+
+    /*Fonction qui renvoie l'index de la case contenant
+      le nombre demandé (en recherche binaire) */
+    public static int RechercheBinaire(int[] Tableau, int A, int Indexmin, int Indexmax) {
+        if (Indexmax >= Indexmin) {
+            int Indexmid = (Indexmin + Indexmax) / 2;
+
+            if (A == Tableau[Indexmid]) {
+                return Indexmid;
+            }
+            if (A < Tableau[Indexmid]) {
+                return RechercheBinaire(Tableau, A, Indexmin, Indexmid - 1);
+            }
+                return RechercheBinaire(Tableau, A, Indexmid + 1, Indexmax);
+
+        }
+        return -1;
+}
 
     public static void main(String[] args) {
 
@@ -16,11 +41,13 @@ public class TP5 {
         scores[5] = 60;
         scores[6] = 70;
 
+        int min = 0;
+        int max = scores.length;
         int IndexNum;
-        IndexNum = RechercheBinaire(scores,80);
+        IndexNum = RechercheBinaire(scores,71,min,max);
 
         System.out.println(IndexNum);
 
-
+        
     }
 }
